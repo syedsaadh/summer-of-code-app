0419cd71156bb397c1a43f020c7d6e30b7e5bd8b
syedsaad-h <hassan.saad.mail@gmail.com>
2016-06-24 12:19:17 +0530

    Broadcast bug persist

--- app/src/main/java/com/example/practiceset5/pset11.java
+++ app/src/main/java/com/example/practiceset5/pset11.java
@@ -1,8 +1,10 @@
 package com.example.practiceset5;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
@@ -15,12 +17,73 @@ public class pset11 extends AppCompatActivity {
     EditText editText1;
     EditText editText2;
 
+    BroadcastReceiver brsent;
+    Intent intesent;
+    PendingIntent pisent;
+    IntentFilter infsent;
+    String sms_sent="Message sent";
+
+    BroadcastReceiver brdel;
+    Intent intedel;
+    PendingIntent pidel;
+    IntentFilter infdel;
+    String sms_del="Message delivered";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pset11);
+        intesent=new Intent(sms_sent);
+        pisent=PendingIntent.getBroadcast(getApplicationContext(),0,intesent,0);
+        infsent=new IntentFilter(sms_sent);
 
+        intedel=new Intent(sms_del);
+        pidel=PendingIntent.getBroadcast(getApplicationContext(),0,intedel,0);
+        infdel=new IntentFilter(sms_del);
+
+        brsent=new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                switch (getResultCode())
+                {
+                    case RESULT_OK:
+                        Toast.makeText(getApplicationContext(),"SMS_SENT",Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        break;
+                    default:
+                }
+            }
+        };
+
+        brdel=new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                switch (getResultCode())
+                {
+                    case RESULT_OK:
+                        Toast.makeText(getApplicationContext(),"SMS_DELIVERED",Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        break;
+                    default:
+                }
+            }
+        };
     }
+    protected void onResume()
+    {
+        super.onResume();
+        registerReceiver(brsent,infsent);
+        registerReceiver(brdel,infdel);
+    }
+    protected void onPause()
+    {
+        super.onPause();
+        unregisterReceiver(brsent);
+        unregisterReceiver(brdel);
+    }
+
     public void SendSMS(View view) {
         editText1 = (EditText)findViewById(R.id.editTextReceiversNo);
         editText2 = (EditText)findViewById(R.id.editTextMessage);
@@ -28,6 +91,8 @@ public class pset11 extends AppCompatActivity {
         String message = editText2.getText().toString();
         SmsManager sms = SmsManager.getDefault();
         sms.sendTextMessage(mobileNumber, null, message, null, null);
+        Toast.makeText(getApplicationContext(),"SMS_SENT",Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(),"SMS_DELIVERED",Toast.LENGTH_SHORT).show();
     }
 
 }
